2014385dcbc0700cdae20ff5216daf3c135cf80c
Joerg Hoppe <j_hoppe@t-online.de>
2016-03-08 08:02:43 +0100

    Removed old PiDP8 server deployment scripts. See http://retrocmp.com/projects/blinkenbone/blinkenbone-physical-panels/249-pidp8-as-blinkenbone-panel and http://retrocmp.com/projects/blinkenbone/blinkenbone-software/250-blinkenbone-blinkenlightapitest

--- projects/11_pidp8/deploy/ubuntu-x86/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "*** Starting Blinkenlight API test Client and connect to server on Raspberry PI"
-chmod a+x blinkenlightapitst
-./blinkenlightapitst raspberrypi
--- projects/11_pidp8/deploy/win32/start.bat
+++
@@ -1,2 +0,0 @@
-@echo Starting Blinkenlight API test Client and connect to server on Raspberry PI
-blinkenlight_test raspberrypi
